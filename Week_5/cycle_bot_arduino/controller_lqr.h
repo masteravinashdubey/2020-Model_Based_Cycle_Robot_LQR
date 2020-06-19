38c3a8224def7fd18d99211ded04e161481f19b0
shreyarastogi <41367451+shreyarastogi@users.noreply.github.com>
2020-06-19 23:14:03 +0530

    Update controller_lqr.h

--- Week_5/cycle_bot_arduino/controller_lqr.h
+++ Week_5/cycle_bot_arduino/controller_lqr.h
@@ -1,7 +1,7 @@
 
 /*Global Variables*/
-float reqVelocity = 0, reqDistance = 0, reqRoll = 0, reqOmega = 0;
-float errorVelocity = 0, errorDistance = 0, errorRoll = 0, errorOmega = 0;
+float reqthetadot = 0, reqtheta = 0, reqphi = 0, reqphidot = 0;
+float errorthetadot = 0, errortheta = 0, errorphi = 0, errorphidot = 0;
 const float angle_offset = (0.01745 * 3);
 int dummy = 0;
 float U = 0, U_new = 0;
@@ -10,26 +10,26 @@ float U = 0, U_new = 0;
 void lqr()
 {
   //Gain matrix optained from octave for sampling time of 7 ms
-  float k[4] = {-25.801483, -3.048010, -0.066327, -0.082783 };
-                               
+  float k[4] = {-25.801483, -3.048010, -0.066327, -0.082783 };     // { distance, velocity, angle, angular velocity }
+
   {
     // When traversing in the normal arena         
     {
       k[1] += k[0];
       k[0] = 0;
-      reqDistance = 0;
-      reqVelocity = 0;
-      reqRoll = 0;
-      reqOmega = 0;
+      reqtheta = 0;
+      reqthetadot = 0;
+      reqphi = 0;
+      reqphidot = 0;
     }
   }
-  
-  errorDistance = (distance - reqDistance);           //Error in the distance covered
-  errorVelocity = (velocity - reqVelocity);           //Error in the velocity
-  errorRoll = (roll - reqRoll) + angle_offset ;       //Error in the tilt angle
-  errorOmega = (omega - reqOmega);                    //Error in angular velocity
 
-  U = ( - k[0] * errorDistance - k[1] * errorVelocity - k[2] * errorRoll - k[3] * errorOmega);
+  errortheta = (theta - reqtheta) + angle_offset ;    //Error in the distance covered
+  errorthetadot = (thetadot - reqthetadot);           //Error in the velocity
+  errorphi = (phi - reqphi) ;                         //Error in the tilt angle
+  errorphidot = (phidot - reqphidot);                 //Error in angular velocity
+
+  U = ( - k[0] * errortheta - k[1] * errorthetadot - k[2] * errorphi - k[3] * errorphidot);
   U_new = constrain(U * 255 / 12 , -255, 255);
 
   {
