2d6d2484e3de4326c7c738b23b6720fa3661668a
Shreya <shreyar686@gmail.com>
2020-06-22 13:20:22 +0530

    corrected

--- WEEK_6/cycle_bot_arduino/controller_lqr.h
+++ WEEK_6/cycle_bot_arduino/controller_lqr.h
@@ -1,4 +1,3 @@
-
 /*Global Variables*/
 float reqthetadot = 0, reqtheta = 0, reqphi = 0, reqphidot = 0;
 float errorthetadot = 0, errortheta = 0, errorphi = 0, errorphidot = 0;
@@ -11,31 +10,32 @@ float U = 0, U_new = 0;
 
 void lqr(CompFil mpu)
 {
-  theta = mpu.roll_deg;
   //Serial.println("in lqr");
   //Gain matrix optained from octave for sampling time of 7 ms
   float k[4] = {-25.801483, -3.048010, -0.066327, -0.082783 };     // { distance, velocity, angle, angular velocity }
-
-  {
+Serial.print("roll= ");
+  Serial.println(mpu.roll_deg);
+  Serial.print("omega= ");
+  Serial.println(mpu.omega);
     // When traversing in the normal arena         
-    {
+    
       k[1] += k[0];
       k[0] = 0;
       reqtheta = 0;
       reqthetadot = 0;
       reqphi = 0;
       reqphidot = 0;
-    }
-  }
-
-  errortheta = (theta - reqtheta) + angle_offset ;    //Error in the distance covered
-  errorthetadot = (thetadot - reqthetadot);           //Error in the velocity
+    
+  
+  errortheta = (mpu.roll_deg - reqtheta) + angle_offset ;    //Error in the distance covered
+  //Serial.println(errortheta);
+  errorthetadot = (mpu.omega - reqthetadot);           //Error in the velocity
   errorphi = (phi - reqphi) ;                         //Error in the tilt angle
   errorphidot = (phidot - reqphidot);                 //Error in angular velocity
 
-  U = ( - k[0] * errortheta - k[1] * errorthetadot - k[2] * errorphi - k[3] * errorphidot);
+  U = (-1* (k[0] * errortheta) - (k[1] * errorthetadot) - (k[2] * errorphi) - (k[3] * errorphidot));
   U_new = constrain(U * 255 / 12 , -255, 255);
-Serial.println(U_new);
+//Serial.println(U_new);
 //  if(U_new >= 0)
 //  {
 //    reaction.setDir(1);
--- WEEK_6/cycle_bot_arduino/cycle_bot_arduino.ino
+++ WEEK_6/cycle_bot_arduino/cycle_bot_arduino.ino
@@ -1,3 +1,4 @@
+@@ -1,125 +0,0 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Wire.h"
@@ -5,7 +6,7 @@
 #include"xbee.h"
 #include"motor.h"
 #include"mpu.h"
-#include"controller_lqr.h"
+
 #include<Servo.h>
 #define PI 3.14159
 #define encPin1 18     //SET PIN NUMBERS
@@ -13,14 +14,14 @@
 #define servoPin 9
 
 XBee mod(&Serial1);  	//mention the name of serial being used to communicate with XBee
-motor reaction(4,5,10);  //change the pin numbers here
-motor drive(6,7,11);     //change the pin numbers here
+motor reaction(4,5,2);  //change the pin numbers here
+motor drive(6,7,3);     //change the pin numbers here
+#include"controller_lqr.h"
 Servo handle;
 CompFil mpu;
 int encoderCount=0,prevCount=0;
 
 long prevtime = 0;
-bool check = 0;
 
 void setup()
 {
@@ -32,36 +33,55 @@ void setup()
   #endif
 
   Serial.begin(115200);
+  
+ mpu.init();
+  Serial.println("going in loop");
   handle.attach(servoPin);
   handle.write(0);
-  attachInterrupt(digitalPinToInterrupt(encPin1),encoderHandler,RISING);
   pinMode(encPin2,INPUT_PULLUP);
+  pinMode(encPin1,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(encPin1),encoderHandler,RISING);
+  
   enable_timer();
-  mpu.init();
+  
+
+ Serial.println("ok");
+
 }
+bool check = 0,check2=0;
+
 
 void loop()
 {
-  Serial.print(mpu.roll_deg);
-  Serial.print("\t");
-  Serial.print(mpu.roll);
-  Serial.print("Omega:\t");
-  Serial.println(mpu.omega);
+  //Serial.println(check);
+  //Serial.println(mpu.roll_deg);
+//  Serial.print("\t");
+  //Serial.println(mpu.roll);
+//  Serial.print("Omega:\t");
+//  Serial.println(mpu.omega);
 
- if ((micros() - prevtime) >= 7000)
+ if ((micros() - prevtime) >= 7)
   {
-    lqr();
+  //  Serial.println("going in lqr");
+ lqr(mpu);
+
     prevtime = micros();
   }
 
   if(check)
   {
- mpu.read_accel();
+ mpu.read_accel(); 
  mpu.read_gyro();
  mpu.complimentary_filter_roll();
  check=0;
   }
-
+if(check2)
+{
+  phidot = (encoderCount - prevCount)*2*PI/ (280*0.01); //in rad/sec
+  prevCount=encoderCount;
+ // Serial.println(phidot);
+	check2=0;
+}
 }
 
 void encoderHandler()
@@ -76,48 +96,31 @@ void encoderHandler()
 
 void enable_timer()
 {
-/*******************ENABELING TIMER TO MEASURE RPM************************************* 
- * TIMER IS USED IN CTC MODE
- * OCR2A = 156 GENERATES INTERRUPT AFTER EVERY 10ms.
- * TIMER IS ENABLES WITH PRESCALER OF 1024.
- * TIMER INTERRUPT IS ENABLED.
- */
-	noInterrupts();          // disable all interrupts
-	TCCR2A = (1<<WGM21);     //CTC mode
-	TCCR2B = 7;              //1024 prescaler
-	OCR2A = 78;             // compare match register, setting for 5ms
-	
-	
-	TIMSK2 = (1 << OCIE2A);  // enable timer compare interrupt
+  noInterrupts();          // disable all interrupts
+  
+  TCCR2A = (1<<WGM21);     //CTC mode
+  TCCR2B = 7;              //1024 prescaler
+  OCR2A = 156;             // compare match register, setting for 10ms
+  TIMSK2 = (1 << OCIE2A);  // enable timer compare interrupt
+  TCNT2  = 0;
 
-	TCNT2  = 0;
+  TCCR0A = (1<<WGM01);            //CTC mode
+  TCCR0B = (1<<CS02)|(1<<CS00);   //1024 prescaler
+  OCR0A = 46;                     // compare match register, setting for 3ms
+  TIMSK0 = (1 << OCIE0A);         // enable timer compare interrupt
+  TCNT0  = 0;
+  
+  interrupts();             // enable all interrupts
+}
+ISR(TIMER2_COMPA_vect)
+{
+  check=1;
 
-	//interrupts();             // enable all interrupts
-	
-	//TIMER0
-	
-	//noInterrupts();          // disable all interrupts
-	TCCR0A = (1<<WGM01);     //CTC mode
-	TCCR0B = 5;              //1024 prescaler
-	OCR0A = 46;             // compare match register, setting for 3ms
-	
-	
-	TIMSK0 = (1 << OCIE0A);  // enable timer compare interrupt
-
-	TCNT0  = 0;
-
-	interrupts();             // enable all interrupts
+  //  PORTJ^=0x01;
 }
 
-ISR(TIMER0_COMPA_VECT)
+ISR(TIMER0_COMPA_vect)
 {
- phidot = (encoderCount - prevCount)*2*PI/ (280*0.01); //in rad/sec
-  prevCount=encoderCount;
-   Serial.println(phidot);
-}
-
-ISR(TIMER2_COMPA_vect)          // timer compare interrupt service routine
-{
-//mpu.testing();
-check=1;
+  check2=1;
+//  PORTJ^=0x02;
 }
