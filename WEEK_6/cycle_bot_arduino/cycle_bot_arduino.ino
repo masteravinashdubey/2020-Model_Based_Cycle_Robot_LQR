eaa0f161ba0bce6dca58c519a2fa514b84e5f0fa
Shreya <shreyar686@gmail.com>
2020-06-22 10:22:15 +0530

    commit

---
+++ WEEK_6/cycle_bot_arduino/cycle_bot_arduino.ino
@@ -0,0 +1,123 @@
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+
+#include"xbee.h"
+#include"motor.h"
+#include"mpu.h"
+#include"controller_lqr.h"
+#include<Servo.h>
+#define PI 3.14159
+#define encPin1 18     //SET PIN NUMBERS
+#define encPin2 19
+#define servoPin 9
+
+XBee mod(&Serial1);  	//mention the name of serial being used to communicate with XBee
+motor reaction(4,5,10);  //change the pin numbers here
+motor drive(6,7,11);     //change the pin numbers here
+Servo handle;
+CompFil mpu;
+int encoderCount=0,prevCount=0;
+
+long prevtime = 0;
+bool check = 0;
+
+void setup()
+{
+
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+
+  Serial.begin(115200);
+  handle.attach(servoPin);
+  handle.write(0);
+  attachInterrupt(digitalPinToInterrupt(encPin1),encoderHandler,RISING);
+  pinMode(encPin2,INPUT_PULLUP);
+  enable_timer();
+  mpu.init();
+}
+
+void loop()
+{
+  Serial.print(mpu.roll_deg);
+  Serial.print("\t");
+  Serial.print(mpu.roll);
+  Serial.print("Omega:\t");
+  Serial.println(mpu.omega);
+
+ if ((micros() - prevtime) >= 7000)
+  {
+    lqr();
+    prevtime = micros();
+  }
+
+  if(check)
+  {
+ mpu.read_accel();
+ mpu.read_gyro();
+ mpu.complimentary_filter_roll();
+ check=0;
+  }
+
+}
+
+void encoderHandler()
+{
+	if(digitalRead(encPin2))
+		encoderCount++;
+	else
+		encoderCount--;
+
+	phi = encoderCount*2*PI/280;
+}
+
+void enable_timer()
+{
+/*******************ENABELING TIMER TO MEASURE RPM************************************* 
+ * TIMER IS USED IN CTC MODE
+ * OCR2A = 156 GENERATES INTERRUPT AFTER EVERY 10ms.
+ * TIMER IS ENABLES WITH PRESCALER OF 1024.
+ * TIMER INTERRUPT IS ENABLED.
+ */
+	noInterrupts();          // disable all interrupts
+	TCCR2A = (1<<WGM21);     //CTC mode
+	TCCR2B = 7;              //1024 prescaler
+	OCR2A = 78;             // compare match register, setting for 5ms
+	
+	
+	TIMSK2 = (1 << OCIE2A);  // enable timer compare interrupt
+
+	TCNT2  = 0;
+
+	//interrupts();             // enable all interrupts
+	
+	//TIMER0
+	
+	//noInterrupts();          // disable all interrupts
+	TCCR0A = (1<<WGM01);     //CTC mode
+	TCCR0B = 5;              //1024 prescaler
+	OCR0A = 46;             // compare match register, setting for 3ms
+	
+	
+	TIMSK0 = (1 << OCIE0A);  // enable timer compare interrupt
+
+	TCNT0  = 0;
+
+	interrupts();             // enable all interrupts
+}
+
+ISR(TIMER0_COMPA_VECT)
+{
+ phidot = (encoderCount - prevCount)*2*PI/ (280*0.01); //in rad/sec
+  prevCount=encoderCount;
+   Serial.println(phidot);
+}
+
+ISR(TIMER2_COMPA_vect)          // timer compare interrupt service routine
+{
+//mpu.testing();
+check=1;
+}
